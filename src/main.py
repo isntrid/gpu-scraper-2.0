5482ab2ea427552ec0240941717e5d3b9a63495f
isntrid <ridn123321@gmail.com>
2026-02-10 11:25:00 +0000

    now supports prices >999

--- src/main.py
+++ src/main.py
@@ -11,16 +11,17 @@ soup = BeautifulSoup(html, "html.parser")
 tags = soup.find_all("strong")
 mappings = {}
 for tag in tags:
-    if tag.getText().isdigit() and int(tag.getText()) >= 90: # need to fix so it can support >999 with commas 
+    price = tag.get_text().replace(",", "")
+    if price.isdigit() and int(price) >= 90:
         container = tag.find_parent("div", class_="item-cell")
         link_tag = container.find("a", class_="item-title")
         link = link_tag.get("href")
         name = link_tag.get_text()
-        price = int(tag.get_text().strip())
+        price = int(price.strip())
         mappings[price] = [link, name]
 
 
-cheapest_price = min(mappings.keys())
+cheapest_price = max(mappings.keys())
 cheapest_link = mappings[cheapest_price][0]
 cheapest_name = mappings[cheapest_price][1]
 print(cheapest_link)
